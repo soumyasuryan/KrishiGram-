24b5081e3af38462e4eff5204a35a18d0de3d37b
Soumya Suryan <soumyasuryan86@gmail.com>
2025-11-21 15:15:16 +0530

    Pest detection updated

--- app/pest_detection/page.js
+++ app/pest_detection/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
+import Script from "next/script";
 import { useState } from "react";
 
 export default function Home() {
@@ -15,10 +16,10 @@ export default function Home() {
             </div>
 
             <div className="w-[70%] mx-auto my-20 min-h-[60%]">
-                <script
+                <Script
                     type="module"
                     src="https://gradio.s3-us-west-2.amazonaws.com/5.49.1/gradio.js"
-                ></script>
+                ></Script>
 
                 <gradio-app src="https://soumyasuryan-disease-predictor.hf.space"></gradio-app>
 
